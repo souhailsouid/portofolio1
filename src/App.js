8f1d99d9b7df530abf6851e0ff8782aa649f01d5
Souhail <mr.souid@live.fr>
2019-07-25 18:40:02 +0200

    update our menu-items to navigate withRouteHOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import React from 'react'
 import './App.css'
 import HomePage from './pages/homepage/homepage.component'
+import { Switch, Route } from 'react-router-dom'
 
+const HatsPage = (props) => (
+	<div>
+		<h1>HATS PAGE</h1>
+	</div>
+)
+
+// const TopicsList = (props) => {
+// 	console.log(props)
+// 	return (
+// 		<div>
+// 			<h1>TOPIC LIST PAGE</h1>
+// 		</div>
+// 	)
+// }
+// const TopicDetail = (props) => {
+// 	console.log(props)
+// 	return (
+// 		<div>
+// 			<h1>TOPIC DETAIL PAGE: {props.match.params.topicId}</h1>
+// 		</div>
+// 	)
+// }
 function App() {
 	return (
 		<div>
-			<HomePage />
+			<Switch>
+				<Route exact path="/" component={HomePage} />
+				{/* <Route exact path="/topics" component={TopicsList} /> */}
+				{/* <Route path="/topics/:topicId" component={TopicDetail} /> */}
+				<Route path="/shop/hats" component={HatsPage} />
+			</Switch>
 		</div>
 	)
 }
