19747043c187f806543632790a3e8049cef3e191
Souhail <mr.souid@live.fr>
2019-07-27 17:31:54 +0200

    create header

---
+++ src/components/collection-preview/collection-preview,styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  'collection-preview': {
+    'display': 'flex',
+    'flexDirection': 'column',
+    'marginBottom': [{ 'unit': 'px', 'value': 30 }]
+  },
+  'collection-preview title': {
+    'fontSize': [{ 'unit': 'px', 'value': 28 }],
+    'marginBottom': [{ 'unit': 'px', 'value': 25 }]
+  },
+  'collection-preview preview': {
+    'display': 'flex',
+    'justifyContent': 'space-between'
+  }
+});
