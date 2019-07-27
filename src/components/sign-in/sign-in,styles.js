a295ff5b26572f75b4fe7b2b2e7772c5bd6f4584
Souhail <mr.souid@live.fr>
2019-07-27 18:41:44 +0200

    add custom  form

---
+++ src/components/sign-in/sign-in,styles.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  'sign-in': {
+    'width': [{ 'unit': 'vw', 'value': 30 }],
+    'display': '"flex"',
+    'flexDirection': 'column'
+  }
+});
